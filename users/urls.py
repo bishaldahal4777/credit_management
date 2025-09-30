6d6c05c8e16d0558fb0ce496e064385b28948a3b
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-30 09:46:43 +0545

    created customer overview html page

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('customers/', views.customer_overview, name='customer_overview'),
 ]
