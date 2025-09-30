0874f1d2e8175611e451d7777427cabfc4db45cc
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-30 09:48:41 +0545

    customer overdiew

--- credits/utils.py
+++ credits/utils.py
@@ -1,7 +1,13 @@
 from django.db.models import Sum
 from .models import Credit, Payment
+from django.utils import timezone
 
 def customer_balance(customer):
     total_credit = Credit.objects.filter(customer=customer).aggregate(Sum('amount'))['amount__sum'] or 0
     total_paid = Payment.objects.filter(credit__customer=customer).aggregate(Sum('amount_paid'))['amount_paid__sum'] or 0
     return total_credit - total_paid
+
+def is_overdue(credit):
+    if not credit.is_paid and credit.due_date < timezone.now().date():
+        return True
+    return False
