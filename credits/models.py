a6ab22e66a9bd543779438f962ae4ae38837abc2
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-30 08:32:54 +0545

    credit-management

--- credits/models.py
+++ credits/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from customers.models import Customer
 
 # Create your models here.
+class Credit(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
+    is_paid = models.BooleanField(default=False)
