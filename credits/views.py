f3690e2912da9d47e277b432f0f56dfbb2888c25
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-30 08:46:49 +0545

    credit forms

--- credits/views.py
+++ credits/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreditForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def add_credit(request):
+    if request.method == 'POST':
+        form = CreditForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('credit_list')  # we will create this view next
+    else:
+        form = CreditForm()
+    return render(request, 'credits/add_credit.html', {'form': form})
