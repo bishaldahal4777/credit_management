3e2a714a7b03f96d7776191b2b940dad5afdcd06
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-30 20:55:39 +0545

    new pages ui

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('customers/', views.customer_overview, name='customer_overview'),
     path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 ]
