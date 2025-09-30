0aa1c3d14bf077b27da0ad48249d4b1bdb5448d4
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-30 08:21:11 +0545

    models of customers

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    shop = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
