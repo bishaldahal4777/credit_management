83618fb29b378dd705715ea0c275a3077bccbb6b
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-30 08:58:05 +0545

    added payments in forms.py

--- credits/forms.py
+++ credits/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Credit
+from .models import Payment
+
 
 class CreditForm(forms.ModelForm):
     class Meta:
         model = Credit
         fields = ['customer', 'amount', 'description', 'due_date', 'is_paid']
+
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = Payment
+        fields = ['credit', 'amount_paid']
--- credits/models.py
+++ credits/models.py
@@ -9,3 +9,11 @@ class Credit(models.Model):
     date = models.DateField(auto_now_add=True)
     due_date = models.DateField()
     is_paid = models.BooleanField(default=False)
+
+class Payment(models.Model):
+    credit = models.ForeignKey(Credit, on_delete=models.CASCADE)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.credit.customer.name} - {self.amount_paid}"
