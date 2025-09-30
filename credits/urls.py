13437594b218651209b9dfc62149ecbc38524d0c
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-30 08:54:34 +0545

    list view added in credit

--- credits/urls.py
+++ credits/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.credit_list, name='credit_list'),
     path('add/', views.add_credit, name='add_credit'),
 ]
--- credits/views.py
+++ credits/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CreditForm
 from django.contrib.auth.decorators import login_required
+from .models import Credit
 
 @login_required
 def add_credit(request):
@@ -12,3 +13,9 @@ def add_credit(request):
     else:
         form = CreditForm()
     return render(request, 'credits/add_credit.html', {'form': form})
+
+@login_required
+def credit_list(request):
+    # Show only credits for customers belonging to the logged-in shop
+    credits = Credit.objects.filter(customer__shop=request.user).order_by('-date')
+    return render(request, 'credits/credit_list.html', {'credits': credits})
