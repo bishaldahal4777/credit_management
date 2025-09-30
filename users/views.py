05956c0585dbf7b8beefeea61415ba67a56a0e8a
Bishal Dahal <bishaldahal4777@gmail.com>
2025-09-30 08:19:36 +0545

    credit_management project setup

---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
